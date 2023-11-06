42dde09d6e35010de3ef4faf81249b0d226d1985
Bilal Shweiki <shweikibilal083@gmail.com>
2023-11-06 11:07:17 +0200

    TickTaeToe project with some bugs

---
+++ Youtube-project/script.js
@@ -0,0 +1,172 @@
+const squaresElem = Array.from(document.querySelectorAll(".square"));
+const turnTextElem = document.getElementById("player-turn");
+const resultTextElem = document.querySelector(".result-text");
+const resetBtnElem = document.getElementById("reset-button");
+const playAiBtnElem = document.getElementById("play-ai");
+
+IN_PROG = "IN_PROG";
+TIE = 'TIE';
+
+scores = {
+    'X': 1,
+    'O': -1,
+    'TIE': 0
+};
+
+let gameEnd = false;
+let emptySquares = 9;
+let gameBoard = [
+    ' ', ' ', ' ',
+    ' ', ' ', ' ',
+    ' ', ' ', ' '
+];
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+let currentTurn = 'X';
+squaresElem.forEach((square, index) => {
+    square.addEventListener("click", function() {
+        addMark(square, index);
+    })
+});
+
+resetBtnElem.addEventListener("click", function() {
+    location.reload();
+});
+
+playAiBtnElem.addEventListener("click", function() {
+    playBestMove(gameBoard, 'X', 'O');
+    squaresElem.forEach((square, index) => {
+        square.addEventListener("click", function() {
+            addMark(square, index);
+            playBestMove(gameBoard, 'X', 'O');
+        })
+    });
+})
+
+function playBestMove(board, aiTurn, humanTurn) {
+    let maxScore = -Infinity;
+    let bestMove;
+    for (let i=0;i<board.length;i++) {
+        if (board[i] === ' ') {
+            board[i] = aiTurn;
+            let score = miniMax(board, humanTurn, false);
+            board[i] = ' ';
+            if (score > maxScore) {
+                maxScore = score;
+                bestMove = i;
+            }
+        }
+    }
+    addMark(squaresElem[bestMove], bestMove);
+}
+
+function evaluateBoard(board) {
+    for (let i=0;i<winPatterns.length;i++) {
+        if (board[winPatterns[i][0]] === ' ' || board[winPatterns[i][1]] === ' ' || board[winPatterns[i][2]] === ' ') {
+            continue;
+        }
+        if (board[winPatterns[i][0]] === board[winPatterns[i][1]] && board[winPatterns[i][1]] === board[winPatterns[i][2]]) {
+            return board[winPatterns[i][0]];
+        }
+    }
+    if (board.includes(' ')) {
+        return IN_PROG;
+    }
+    return 'TIE';
+}
+
+function miniMax(board, currTurn, isMaximizing) {
+    let result = evaluateBoard(board);
+    if (result !== IN_PROG) {
+        return scores[result];
+    }
+
+    if (isMaximizing) {
+        let maxEval = -Infinity;
+        for (let i=0;i<board.length;i++) {
+            if (board[i] === ' '){
+                board[i] = currTurn;
+                nextTurn = currTurn === 'O' ? 'X' : 'O';
+                let eval = miniMax(board, nextTurn, false);
+                board[i] = ' ';
+                maxEval = Math.max(eval, maxEval);
+            }
+        }
+        return maxEval;
+    }
+
+    else {
+        let minEval = Infinity;
+        for (let i=0;i<board.length;i++) {
+            if (board[i] === ' '){
+                board[i] = currTurn;
+                nextTurn = currTurn === 'O' ? 'X' : 'O';
+                let eval = miniMax(board, nextTurn, true);
+                board[i] = ' ';
+                minEval = Math.min(eval, minEval);
+            }
+        }
+        return minEval;
+    }
+}
+
+
+
+function addMark(square, index) {
+    if (gameEnd || !square.classList.contains("empty")) {
+        return;
+    }
+    square.classList.remove("empty");
+    emptySquares--;
+    square.innerHTML = currentTurn;
+    gameBoard[index] = currentTurn;
+    let currGameResult = checkWinCondition();
+    if (gameEnd) {
+        announceResult(currGameResult);
+        return;
+    }
+    changeTurn();
+}
+
+function changeTurn() {
+    currentTurn = currentTurn === 'X' ? 'O' : 'X';
+    turnTextElem.textContent = currentTurn;
+}
+
+function checkWinCondition() {
+    for (let i=0;i<winPatterns.length;i++) {
+        if (gameBoard[winPatterns[i][0]] === ' ' || gameBoard[winPatterns[i][1]] === ' ' || gameBoard[winPatterns[i][2]] === ' ' ) {
+            continue;
+        }
+        if (gameBoard[winPatterns[i][0]] === gameBoard[winPatterns[i][1]] && gameBoard[winPatterns[i][1]] === gameBoard[winPatterns[i][2]]) {
+            gameEnd = true;
+            return gameBoard[winPatterns[i][0]];
+        }
+        if (emptySquares === 0) {
+            gameEnd = true;
+            return TIE;
+        }
+    }
+    return null;
+}
+
+
+function announceResult(gameResult) {
+    if (gameResult === TIE) {
+        resultTextElem.innerHTML = `It's a Tie!`
+    }
+    else {
+        resultTextElem.innerHTML = `Player ${gameResult} Won!`;
+    }
+    resultTextElem.classList.remove("hidden");
+}
